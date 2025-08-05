45e01cc3acb6769617ab89c517d458f87116276f
Kristina <vlasovak180@gmail.com>
2025-08-05 22:16:44 +0300

    ершкв commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/test_task_infotecs" : "",
-  assetPrefix: isProd ? "/test_task_infotecs/" : "",
+  basePath: "/test_task_infotecs",
+
   reactStrictMode: true,
 };
 
