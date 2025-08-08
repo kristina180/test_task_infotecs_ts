2580270f7644eeabbdf0c0039e9f70397f55dd04
Kristina <vlasovak180@gmail.com>
2025-08-08 16:34:39 +0300

    second commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  // basePath: "/test_task_infotecs",
+  output: "export",
+  basePath: "/test_task_infotecs",
 
   reactStrictMode: true,
 };
